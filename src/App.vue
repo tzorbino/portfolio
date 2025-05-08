d89c5894a25f3ec63aa482eabd68e1c296dcb5fa
Tony Zorbino <tzorbino@gmail.com>
2025-05-08 13:10:34 -0400

    updated resume

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       <router-link to="/projects" class="nav-link">Projects</router-link>
       <router-link to="/metacognitives" class="nav-link">Metacognitives</router-link>
       <router-link to="/contact" class="nav-link">Contact</router-link>
-      <a href="/Zorbino_Resume.pdf" target="_blank" class="nav-link">Resume</a>
+      <a href="/Antonio_Zorbino_Resume.pdf" target="_blank" class="nav-link">Resume</a>
     </nav>
 
     <div class="page-content">
